ec21130e5f200c00df1067499b0c9b08a2ed9491
bharadwaj palakurthy <bharadwajpalakurthy@gmail.com>
2024-05-23 00:57:36 +0530

    feat: removed explore page

--- components/nav_bar/navBar.tsx
+++ components/nav_bar/navBar.tsx
@@ -17,117 +17,117 @@ import Icon from "../icon/icon";
 import useScreenSize from "@/hooks/helpers/useScreenSize";
 
 const NavBar = () => {
-  // This is used to connect safe as wallet,
-  // if the app is opened in the safe context.
-  useAutoConnect();
-  const { isMobile } = useScreenSize();
-  const currentPath = usePathname();
-  const searchParams = useSearchParams();
-  const { signer } = useCantoSigner();
-  const [isMenuOpen, setIsMenuOpen] = useState(false);
-  const [isMoreModalOpen, setIsMoreModalOpen] = useState(false);
+	// This is used to connect safe as wallet,
+	// if the app is opened in the safe context.
+	useAutoConnect();
+	const { isMobile } = useScreenSize();
+	const currentPath = usePathname();
+	const searchParams = useSearchParams();
+	const { signer } = useCantoSigner();
+	const [isMenuOpen, setIsMenuOpen] = useState(false);
+	const [isMoreModalOpen, setIsMoreModalOpen] = useState(false);
 
-  useEffect(() => {
-    if (signer?.account.address) {
-      Analytics.actions.people.registerWallet(signer.account.address);
-      Analytics.actions.identify(signer.account.address, {
-        account: signer.account.address,
-      });
-      Analytics.actions.events.connections.walletConnect(true);
-    }
-  }, [signer]);
+	useEffect(() => {
+		if (signer?.account.address) {
+			Analytics.actions.people.registerWallet(signer.account.address);
+			Analytics.actions.identify(signer.account.address, {
+				account: signer.account.address,
+			});
+			Analytics.actions.events.connections.walletConnect(true);
+		}
+	}, [signer]);
 
-  useEffect(() => {
-    let url = window.origin + currentPath;
-    if (searchParams.toString()) {
-      url += `?${searchParams.toString()}`;
-    }
-    if (currentPath == "/bridge") {
-      Analytics.actions.events.pageOpened("bridge", url);
-    } else if (currentPath == "/lending") {
-      Analytics.actions.events.pageOpened("lending", url);
-    } else if (currentPath == "/lp") {
-      Analytics.actions.events.pageOpened("lp interface", url);
-    } else if (currentPath == "/explore") {
-      Analytics.actions.events.pageOpened("explore", url);
-    } else if (currentPath == "/staking") {
-      Analytics.actions.events.pageOpened("staking", url);
-    } else if (currentPath == "/governance") {
-      Analytics.actions.events.pageOpened("governance", url);
-    } else {
-      Analytics.actions.events.pageOpened("home", url);
-    }
-    isMenuOpen && setIsMenuOpen(false);
-  }, [currentPath, searchParams, signer]);
-  const balance = useBalance({
-    address: signer?.account.address,
-    watch: true,
-    chainId: signer?.chain.id,
-  });
+	useEffect(() => {
+		let url = window.origin + currentPath;
+		if (searchParams.toString()) {
+			url += `?${searchParams.toString()}`;
+		}
+		if (currentPath == "/bridge") {
+			Analytics.actions.events.pageOpened("bridge", url);
+		} else if (currentPath == "/lending") {
+			Analytics.actions.events.pageOpened("lending", url);
+		} else if (currentPath == "/lp") {
+			Analytics.actions.events.pageOpened("lp interface", url);
+		} else if (currentPath == "/explore") {
+			Analytics.actions.events.pageOpened("explore", url);
+		} else if (currentPath == "/staking") {
+			Analytics.actions.events.pageOpened("staking", url);
+		} else if (currentPath == "/governance") {
+			Analytics.actions.events.pageOpened("governance", url);
+		} else {
+			Analytics.actions.events.pageOpened("home", url);
+		}
+		isMenuOpen && setIsMenuOpen(false);
+	}, [currentPath, searchParams, signer]);
+	const balance = useBalance({
+		address: signer?.account.address,
+		watch: true,
+		chainId: signer?.chain.id,
+	});
 
-  return (
-    <div className={styles.container}>
-      <div className={styles.logo}>
-        <button
-          className={styles.menu}
-          onClick={() => {
-            setIsMenuOpen(!isMenuOpen);
-          }}
-        >
-          <Icon
-            icon={{
-              url: "/menu.svg",
-              size: 56,
-            }}
-            themed
-          />
-        </button>
-        <Link href="/">
-          <Image
-            src="/tokens/canto.svg"
-            width={40}
-            height={40}
-            alt="canto"
-            style={{
-              filter: "invert(var(--dark-mode))",
-            }}
-          />
-        </Link>
-      </div>
+	return (
+		<div className={styles.container}>
+			<div className={styles.logo}>
+				<button
+					className={styles.menu}
+					onClick={() => {
+						setIsMenuOpen(!isMenuOpen);
+					}}
+				>
+					<Icon
+						icon={{
+							url: "/menu.svg",
+							size: 56,
+						}}
+						themed
+					/>
+				</button>
+				<Link href="/">
+					<Image
+						src="/tokens/canto.svg"
+						width={40}
+						height={40}
+						alt="canto"
+						style={{
+							filter: "invert(var(--dark-mode))",
+						}}
+					/>
+				</Link>
+			</div>
 
-      <div className={styles["nav-links"]} data-menu-open={isMenuOpen}>
-        <Link
-          href="/bridge"
-          className={clsx(
-            styles["nav-link"],
-            currentPath == "/bridge" && styles.active
-          )}
-          onClick={() => Analytics.actions.events.clickedNavLink("Bridge")}
-        >
-          <Text size="sm">Bridge</Text>
-        </Link>
+			<div className={styles["nav-links"]} data-menu-open={isMenuOpen}>
+				<Link
+					href="/bridge"
+					className={clsx(
+						styles["nav-link"],
+						currentPath == "/bridge" && styles.active
+					)}
+					onClick={() => Analytics.actions.events.clickedNavLink("Bridge")}
+				>
+					<Text size="sm">Bridge</Text>
+				</Link>
 
-        <Link
-          href="/lending"
-          className={clsx(
-            styles["nav-link"],
-            currentPath == "/lending" && styles.active
-          )}
-          onClick={() => Analytics.actions.events.clickedNavLink("Lending")}
-        >
-          <Text size="sm">Lending</Text>
-        </Link>
-        <Link
-          href="/lp"
-          className={clsx(
-            styles["nav-link"],
-            currentPath == "/lp" && styles.active
-          )}
-          onClick={() => Analytics.actions.events.clickedNavLink("Pools")}
-        >
-          <Text size="sm">Pools</Text>
-        </Link>
-        <Link
+				<Link
+					href="/lending"
+					className={clsx(
+						styles["nav-link"],
+						currentPath == "/lending" && styles.active
+					)}
+					onClick={() => Analytics.actions.events.clickedNavLink("Lending")}
+				>
+					<Text size="sm">Lending</Text>
+				</Link>
+				<Link
+					href="/lp"
+					className={clsx(
+						styles["nav-link"],
+						currentPath == "/lp" && styles.active
+					)}
+					onClick={() => Analytics.actions.events.clickedNavLink("Pools")}
+				>
+					<Text size="sm">Pools</Text>
+				</Link>
+				{/* <Link
           href="https://explore.canto.io/"
           className={clsx(
             styles["nav-link"],
@@ -136,102 +136,102 @@ const NavBar = () => {
           onClick={() => Analytics.actions.events.clickedNavLink("Explore")}
         >
           <Text size="sm">Explore</Text>
-        </Link>
-        {isMobile && (
-          <Link
-            href="/staking"
-            className={clsx(
-              styles["nav-link"],
-              currentPath == "/staking" && styles.active
-            )}
-            onClick={() => Analytics.actions.events.clickedNavLink("Staking")}
-          >
-            <Text size="sm">Staking</Text>
-          </Link>
-        )}
-        {isMobile && (
-          <Link
-            href="/governance"
-            className={clsx(
-              styles["nav-link"],
-              (currentPath == "/governance" ||
-                currentPath == "/governance/proposal") &&
-                styles.active
-            )}
-            onClick={() =>
-              Analytics.actions.events.clickedNavLink("Governance")
-            }
-          >
-            <Text size="sm">Governance</Text>
-          </Link>
-        )}
-        {!isMobile && (
-          <div
-            className={styles.moreLink}
-            onMouseEnter={() => setIsMoreModalOpen(true)}
-            onMouseLeave={() => setIsMoreModalOpen(false)}
-          >
-            <div className={styles.moreButtonContainer}>
-              <Text size="sm">More</Text>
-              <div className={styles.dropdown}>
-                <Icon
-                  icon={{
-                    url: "/dropdown.svg",
-                    size: 16,
-                  }}
-                  themed
-                />
-              </div>
-            </div>
-            {isMoreModalOpen && (
-              <div className={styles.popUp}>
-                {
-                  <Link
-                    href="/staking"
-                    className={clsx(styles["dropdownlink"])}
-                    onClick={() => {
-                      setIsMoreModalOpen(false);
-                      Analytics.actions.events.clickedNavLink("Staking");
-                    }}
-                  >
-                    <div>
-                      <Text size="sm">Staking</Text>
-                    </div>
-                  </Link>
-                }
-                {
-                  <Link
-                    href="/governance"
-                    className={clsx(styles["dropdownlink"])}
-                    onClick={() => {
-                      setIsMoreModalOpen(false);
-                      Analytics.actions.events.clickedNavLink("Governance");
-                    }}
-                    style={{ borderBottom: "none" }}
-                  >
-                    <div>
-                      <Text size="sm">Governance</Text>
-                    </div>
-                  </Link>
-                }
-              </div>
-            )}
-          </div>
-        )}
-      </div>
-      <div className={styles["btn-grp"]}>
-        <div className={styles.theme}>
-          <ThemeButton />
-        </div>
-        <div className={styles.activity}>
-          <TransactionModal />
-        </div>
-        <div className={styles["wallet-connect"]}>
-          <ConnectButton key={balance.data?.formatted} chainStatus={"none"} />
-        </div>
-      </div>
-    </div>
-  );
+        </Link> */}
+				{isMobile && (
+					<Link
+						href="/staking"
+						className={clsx(
+							styles["nav-link"],
+							currentPath == "/staking" && styles.active
+						)}
+						onClick={() => Analytics.actions.events.clickedNavLink("Staking")}
+					>
+						<Text size="sm">Staking</Text>
+					</Link>
+				)}
+				{isMobile && (
+					<Link
+						href="/governance"
+						className={clsx(
+							styles["nav-link"],
+							(currentPath == "/governance" ||
+								currentPath == "/governance/proposal") &&
+								styles.active
+						)}
+						onClick={() =>
+							Analytics.actions.events.clickedNavLink("Governance")
+						}
+					>
+						<Text size="sm">Governance</Text>
+					</Link>
+				)}
+				{!isMobile && (
+					<div
+						className={styles.moreLink}
+						onMouseEnter={() => setIsMoreModalOpen(true)}
+						onMouseLeave={() => setIsMoreModalOpen(false)}
+					>
+						<div className={styles.moreButtonContainer}>
+							<Text size="sm">More</Text>
+							<div className={styles.dropdown}>
+								<Icon
+									icon={{
+										url: "/dropdown.svg",
+										size: 16,
+									}}
+									themed
+								/>
+							</div>
+						</div>
+						{isMoreModalOpen && (
+							<div className={styles.popUp}>
+								{
+									<Link
+										href="/staking"
+										className={clsx(styles["dropdownlink"])}
+										onClick={() => {
+											setIsMoreModalOpen(false);
+											Analytics.actions.events.clickedNavLink("Staking");
+										}}
+									>
+										<div>
+											<Text size="sm">Staking</Text>
+										</div>
+									</Link>
+								}
+								{
+									<Link
+										href="/governance"
+										className={clsx(styles["dropdownlink"])}
+										onClick={() => {
+											setIsMoreModalOpen(false);
+											Analytics.actions.events.clickedNavLink("Governance");
+										}}
+										style={{ borderBottom: "none" }}
+									>
+										<div>
+											<Text size="sm">Governance</Text>
+										</div>
+									</Link>
+								}
+							</div>
+						)}
+					</div>
+				)}
+			</div>
+			<div className={styles["btn-grp"]}>
+				<div className={styles.theme}>
+					<ThemeButton />
+				</div>
+				<div className={styles.activity}>
+					<TransactionModal />
+				</div>
+				<div className={styles["wallet-connect"]}>
+					<ConnectButton key={balance.data?.formatted} chainStatus={"none"} />
+				</div>
+			</div>
+		</div>
+	);
 };
 
 export default NavBar;
